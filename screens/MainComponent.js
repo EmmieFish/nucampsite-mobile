814492135435844a4c1f7cbce967cd62545ff0c8
EmmieFish <105821167+EmmieFish@users.noreply.github.com>
2023-04-27 18:38:24 -0700

    Main Component and Directory

---
+++ screens/MainComponent.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { CAMPSITES } from '../shared/campsites/';
+import DirectoryScreen from './DirectoryScreen';
+
+const Main = () => {
+    const [campsites, setCampsties] = useState(CAMPSITES);
+
+    return <DirectoryScreen campsites={campsites} />;
+}
+export default Main
