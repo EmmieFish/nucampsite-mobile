9a7a3ee07978a770fd7a109d69554d47a65aea2a
EmmieFish <105821167+EmmieFish@users.noreply.github.com>
2023-05-11 20:37:00 -0700

    added panresponve animation

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { StyleSheet, Text, View, PanResponder, Alert } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { baseUrl } from "../../shared/baseUrl";
@@ -6,10 +7,17 @@ import * as Animatable from "react-native-animatable";
 const RenderCampsite = (props) => {
     const { campsite } = props;
 
-    const isLeftSwipe = ({ dx }) => dx < -200
+    const view = useRef();
+
+    const isLeftSwipe = ({ dx }) => dx < -100
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current
+                .rubberBand(500)
+                .then((endState) => console.log(endState.finished ? 'finished' : 'canceled'))
+        },
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState)
             if (isLeftSwipe(gestureState)) {
@@ -39,6 +47,7 @@ const RenderCampsite = (props) => {
                 animation="fadeInDownBig"
                 duration={2000}
                 delay={1000}
+                ref={view}
                 {...panResponder.panHandlers}
             >
                 <Card containerStyle={styles.cardContainer}>
