35d705b02ba3e63b007d0ff2f18dcd50cad40883
EmmieFish <105821167+EmmieFish@users.noreply.github.com>
2023-05-16 19:05:12 -0700

    added register form

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,42 +1,44 @@
 import { useEffect, useState } from "react";
 import { View, StyleSheet, ScrollView } from "react-native";
 import { CheckBox, Input, Icon, Button } from "react-native-elements";
-import * as SecureStore from "expo-secure-store"
+import * as SecureStore from "expo-secure-store";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const LoginTab = ({ navigation }) => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [remember, setRemember] = useState(false)
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [remember, setRemember] = useState(false);
 
     const handleLogin = () => {
-        console.log('username: ', username)
-        console.log("password: ", password)
-        console.log("remember: ", remember)
+        console.log("username: ", username);
+        console.log("password: ", password);
+        console.log("remember: ", remember);
 
         if (remember) {
             SecureStore.setItemAsync(
-                'userinfo',
+                "userinfo",
                 JSON.stringify({
                     username,
-                    password
+                    password,
                 })
-            ).catch((error) => console.log('Could not save user info:', error))
+            ).catch((error) => console.log("Could not save user info:", error));
         } else {
-            SecureStore.deleteItemAsync('userinfo').catch((error) => console.log('Could not delete user info:', error))
+            SecureStore.deleteItemAsync("userinfo").catch((error) =>
+                console.log("Could not delete user info:", error)
+            );
         }
-    }
+    };
 
     useEffect(() => {
-        SecureStore.getItemAsync('userinfo').then((userdata) => {
-            const userinfo = JSON.parse(userdata)
+        SecureStore.getItemAsync("userinfo").then((userdata) => {
+            const userinfo = JSON.parse(userdata);
             if (userinfo) {
-                setUsername(userinfo.username)
-                setPassword(userinfo.password)
-                setRemember(true)
+                setUsername(userinfo.username);
+                setPassword(userinfo.password);
+                setRemember(true);
             }
-        })
-    }, [])
+        });
+    }, []);
 
     return (
         <View style={styles.container}>
@@ -81,7 +83,7 @@ const LoginTab = ({ navigation }) => {
             </View>
             <View style={styles.formButton}>
                 <Button
-                    onPress={() => navigation.navigate('Register')}
+                    onPress={() => navigation.navigate("Register")}
                     title="Register"
                     type="clear"
                     icon={
@@ -97,23 +99,122 @@ const LoginTab = ({ navigation }) => {
             </View>
         </View>
     );
-    
-}
+};
 
 const RegisterTab = () => {
-    return <ScrollView></ScrollView>
-}
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [remember, setRemember] = useState(false);
 
-const Tab = createBottomTabNavigator()
+    const handleRegister = () => {
+        const userInfo = {
+            username,
+            password,
+            firstName,
+            lastName,
+            remember,
+        };
+        console.log(JSON.stringify(userInfo))
+
+        if (remember) {
+            SecureStore.setItemAsync(
+                "userinfo",
+                JSON.stringify({
+                    username,
+                    password,
+                })
+            ).catch((error) => console.log("Could not save user info:", error));
+        } else {
+            SecureStore.deleteItemAsync("userinfo").catch((error) =>
+                console.log("Could not delete user info:", error)
+            );
+        }
+    };
+
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <Input
+                    placeholder="Username"
+                    leftIcon={{ type: "font-awesome", name: "user-o" }}
+                    onChangeText={(text) => setUsername(text)}
+                    value={username}
+                    containerStyle={styles.formInput}
+                    leftIconContainerStyle={styles.formIcon}
+                />
+                <Input
+                    placeholder="Password"
+                    leftIcon={{ type: "font-awesome", name: "key" }}
+                    onChangeText={(text) => setPassword(text)}
+                    value={password}
+                    containerStyle={styles.formInput}
+                    leftIconContainerStyle={styles.formIcon}
+                />
+                <Input
+                    placeholder="First Name"
+                    leftIcon={{ type: "font-awesome", name: "user-o" }}
+                    onChangeText={(text) => setFirstName(text)}
+                    value={firstName}
+                    containerStyle={styles.formInput}
+                    leftIconContainerStyle={styles.formIcon}
+                />
+                <Input
+                    placeholder="Last Name"
+                    leftIcon={{ type: "font-awesome", name: "user" }}
+                    onChangeText={(text) => setLastName(text)}
+                    value={lastName}
+                    containerStyle={styles.formInput}
+                    leftIconContainerStyle={styles.formIcon}
+                />
+                <Input
+                    placeholder="Email"
+                    leftIcon={{ type: "font-awesome", name: "envelope-o" }}
+                    onChangeText={(text) => setEmail(text)}
+                    value={email}
+                    containerStyle={styles.formInput}
+                    leftIconContainerStyle={styles.formIcon}
+                />
+                <CheckBox
+                    title="Remember Me"
+                    center
+                    checked={remember}
+                    onPress={() => setRemember(!remember)}
+                    containerStyle={styles.formCheckbox}
+                />
+                <View style={styles.formButton}>
+                    <Button
+                        onPress={() => handleRegister()}
+                        title="Register"
+                        color="#5637DD"
+                        icon={
+                            <Icon
+                                name="user-plus"
+                                type="font-awesome"
+                                color="#fff"
+                                iconStyle={{ marginRight: 10 }}
+                            />
+                        }
+                        buttonStyle={{ backgroundColor: "#5637DD" }}
+                    />
+                </View>
+            </View>
+        </ScrollView>
+    );
+};
+
+const Tab = createBottomTabNavigator();
 
 const LoginScreen = () => {
     const tabBarOptions = {
-        activeBackgroundColor: '#5637DD',
-        inactiveBackgroundColor: '#CEC8FF',
-        activeTintColor: '#fff',
-        inactiveTintColor: '#808080',
-        labelStyle: { fontSize: 16 }
-    }
+        activeBackgroundColor: "#5637DD",
+        inactiveBackgroundColor: "#CEC8FF",
+        activeTintColor: "#fff",
+        inactiveTintColor: "#808080",
+        labelStyle: { fontSize: 16 },
+    };
 
     return (
         <Tab.Navigator tabBarOptions={tabBarOptions}>
@@ -149,26 +250,29 @@ const LoginScreen = () => {
             />
         </Tab.Navigator>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
         justifyContent: "center",
-        margin: 20,
+        margin: 10,
     },
     formIcon: {
         marginRight: 10,
     },
     formInput: {
-        padding: 10,
+        padding: 8,
+        height: 60
     },
     formCheckbox: {
-        margin: 10,
+        margin: 8,
         backgroundColor: null,
     },
     formButton: {
-        margin: 40,
+        margin: 20,
+        marginRight: 40,
+        marginLeft: 40,
     },
 });
 
-export default LoginScreen
+export default LoginScreen;
